35858848ef9803e42791d496d031e8c63ab50141
Finn Then <finnmortenthen@gmail.com>
2022-05-28 12:43:00 +0200

    switched property field from serializer to model

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,22 +1,13 @@
-from datetime import date
 from rest_framework import serializers
 from ..models import Author, Book
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    age = serializers.SerializerMethodField()
+    age = serializers.ReadOnlyField()
 
     class Meta:
         model = Author
         fields = '__all__'
-    
-    def get_age(self, instance):
-        if instance.passed:
-            delta = instance.died - instance.born
-            return delta.days // 365
-        else:
-            delta = date.today() - instance.born
-            return delta.days // 365
 
 
 class BookSerializer(serializers.ModelSerializer):
